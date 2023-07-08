b92120e9c79875e691195442540a569d968d85cd
JostinDev <justinnydegger@hotmail.com>
2023-07-08 22:16:58 +0200

    Fix hero on dev server

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,19 +7,19 @@ import nytz from "../../public/nytz.svg";
 export default function Hero() {
 
 	return (
-			<div className='flex h-[96%] w-full overflow-hidden whitespace-nowrap rotating-gradient rounded-3xl select-none'>
-				<div className='flex w-screen whitespace-nowrap absolute bottom-80 left-0 scale-[1.42]'>
-					<Image alt='studio' src={studio} className="filterSVG animate-leftToRightStudio"/>
-					<Image alt='studio' src={studio} className="filterSVG animate-leftToRightStudio"/>
-					<Image alt='studio' src={studio} className="filterSVG animate-leftToRightStudio"/>
-					<Image alt='studio' src={studio} className="filterSVG animate-leftToRightStudio"/>
+			<div className='flex h-[96%] w-screen overflow-hidden whitespace-nowrap rotating-gradient rounded-3xl select-none'>
+				<div className='flex w-screen whitespace-nowrap absolute bottom-80 left-0 scale-[1.2] animate-leftToRightStudio'>
+					<Image alt='studio' src={studio} className="scale-[1.25]"/>
+					<Image alt='studio' src={studio} className="scale-[1.25]"/>
+					<Image alt='studio' src={studio} className="scale-[1.25]"/>
+					<Image alt='studio' src={studio} className="scale-[1.25]"/>
 				</div>
 
-				<div className='flex w-screen whitespace-nowrap absolute bottom-28 left-0'>
-					<Image alt='studio' src={nytz} className="filterSVG animate-leftToRightNytz"/>
-					<Image alt='studio' src={nytz} className="filterSVG animate-leftToRightNytz"/>
-					<Image alt='studio' src={nytz} className="filterSVG animate-leftToRightNytz"/>
-					<Image alt='studio' src={nytz} className="filterSVG animate-leftToRightNytz"/>
+				<div className='flex w-screen whitespace-nowrap absolute bottom-28 left-0 animate-leftToRightNytz'>
+					<Image alt='studio' src={nytz}/>
+					<Image alt='studio' src={nytz}/>
+					<Image alt='studio' src={nytz}/>
+					<Image alt='studio' src={nytz}/>
 				</div>
 			</div>
 	);
