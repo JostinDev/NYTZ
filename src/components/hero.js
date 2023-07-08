55eada53c2b4dcf8681153c7ca1d6692d8390d5f
JostinDev <justinnydegger@hotmail.com>
2023-07-08 22:20:04 +0200

    Fix hero on dev server

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,15 +7,15 @@ import nytz from "../../public/nytz.svg";
 export default function Hero() {
 
 	return (
-			<div className='flex h-[96%] w-screen overflow-hidden whitespace-nowrap rotating-gradient rounded-3xl select-none'>
-				<div className='flex w-screen whitespace-nowrap absolute bottom-80 left-0 scale-[1.2] animate-leftToRightStudio'>
-					<Image alt='studio' src={studio} className="scale-[1.25]"/>
-					<Image alt='studio' src={studio} className="scale-[1.25]"/>
-					<Image alt='studio' src={studio} className="scale-[1.25]"/>
-					<Image alt='studio' src={studio} className="scale-[1.25]"/>
+			<div className='flex h-[96%] w-full overflow-hidden whitespace-nowrap rotating-gradient rounded-3xl select-none'>
+				<div className='flex w-screen whitespace-nowrap absolute bottom-40 sm:bottom-60 left-0  animate-leftToRightStudio' >
+					<Image alt='studio' src={studio} className="scale-[1.2]"/>
+					<Image alt='studio' src={studio} className="scale-[1.2]"/>
+					<Image alt='studio' src={studio} className="scale-[1.2]"/>
+					<Image alt='studio' src={studio} className="scale-[1.2]"/>
 				</div>
 
-				<div className='flex w-screen whitespace-nowrap absolute bottom-28 left-0 animate-leftToRightNytz'>
+				<div className='flex w-screen whitespace-nowrap absolute bottom-20 left-0 animate-leftToRightNytz'>
 					<Image alt='studio' src={nytz}/>
 					<Image alt='studio' src={nytz}/>
 					<Image alt='studio' src={nytz}/>
