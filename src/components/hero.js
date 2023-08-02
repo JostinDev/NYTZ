a32a21ffef9cd51bc3d42428691c529c0e63227b
JostinDev <justinnydegger@hotmail.com>
2023-08-02 07:13:45 +0200

    Solution for gif

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Hero() {
 
 	return (
 			<div className='h-[96%] rotating-gradient rounded-3xl select-none'>
-				<img  src='/images/please-work.gif' className="min-w-[1500px] w-full left-0 bottom-20 absolute">
-				</img>
+				<Image alt='' width={500} height={500}  src='/images/moving.gif' className="min-w-[1200px] w-full left-0 bottom-20 absolute">
+				</Image>
 			</div>
 	);
 }
